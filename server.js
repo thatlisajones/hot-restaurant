d9c3c7cdb1fed9c1592d996e5e839156aed58164
That Lisa <thatlisajones@gmail.com>
2018-12-22 14:26:16 -0700

    Solution files for in-class activity

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var apiRoutes = require("./app/routes/apiRoutes");
+var htmlRoutes = require("./app/routes/htmlRoutes");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
